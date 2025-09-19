3625c736f7427bb5ae6279b7f8181a34c7e935cf
kyteidev <89528285+kyteidev@users.noreply.github.com>
2025-09-19 18:06:34 +0300

    feat: add interaction to bought command

--- commands/utility/bought.js
+++ commands/utility/bought.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { ButtonBuilder, ButtonStyle, SlashCommandBuilder, ActionRowBuilder } = require('discord.js');
 const { Croissants } = require('../../index.js');
 
 module.exports = {
@@ -17,14 +17,53 @@ module.exports = {
                 await user.update({ croissant_count: user.croissant_count + 1, last_bought: new Date() });
                 return interaction.reply(`Thanks for buying a croissant, ${username}! You have bought ${user.croissant_count} croissants in total.`);
             } else {
-                // Create a new entry
-                console.log('Creating new user entry for', username);
-                await Croissants.create({
-                    username: username,
-                    croissant_count: 1,
-                    last_bought: new Date(),
+                const confirm = new ButtonBuilder()
+                    .setCustomId('confirm')
+                    .setLabel('I swear')
+                    .setStyle(ButtonStyle.Success);
+
+                const cancel = new ButtonBuilder()
+                    .setCustomId('cancel')
+                    .setLabel('Nuh uh')
+                    .setStyle(ButtonStyle.Danger);
+
+                const buttons = new ActionRowBuilder()
+                    .addComponents(confirm, cancel);
+
+                console.log("Asking for confirmation");
+                const response = await interaction.reply({
+                    content: `Do you swear you will only execute this command if you actually bought a croissant, ${username}?`,
+                    components: [buttons],
+                    withResponse: true,
                 });
-                return interaction.reply(`Thanks for starting to buy croissants, ${username}! You now have bought 1 croissant.`);
+
+                const collectorFilter = i => i.user.id === interaction.user.id;
+
+                try {
+                    const confirmation = await response.resource.message.awaitMessageComponent({ filter: collectorFilter, time: 60_000 });
+
+                    if (confirmation.customId === 'confirm') {
+                        // Create a new entry
+                        console.log('Creating new user entry for', username);
+                        await Croissants.create({
+                            username: username,
+                            croissant_count: 1,
+                            last_bought: new Date(),
+                        });
+
+                        return interaction.editReply({
+                            content: `Thanks for starting to buy croissants, ${username}! You now have bought 1 croissant.`,
+                            components: [],
+                        });
+                    } else if (confirmation.customId === 'cancel') {
+                        return interaction.editReply({
+                            content: `Thought so. No croissants for you, ${username}.`,
+                            components: [],
+                        });
+                    };   
+                } catch {
+                    await interaction.editReply({ content: 'Why did you ghost me :(', components: [] });
+                }
             }
         }
         catch (error) {
